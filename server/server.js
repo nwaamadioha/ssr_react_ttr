dba7ade8ee7224c0e0ab1a22db5597f026e1b4cd
nwaamadioha <udokwu.temple960@gmail.com>
2022-06-16 06:48:34 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "../src/App";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use("^/$", (req, res, next) => {
+    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+        if(err){
+            console.log(err)
+            return res.status(500).send("Something went wrong");
+        }
+        return res.send(
+            data.replace(
+                '<div id="root"></div>', 
+                `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+            )
+        );
+    });
+});
+
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
